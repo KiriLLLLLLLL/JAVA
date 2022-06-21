b7c2a250e1f1e5f7101c71136bc96a5a8db6a698
yanax <yanax@mail.ru>
2022-06-21 13:08:58 +0300

    add exp

--- PhoneBook/exportContact.java
+++ PhoneBook/exportContact.java
@@ -1,21 +1,37 @@
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class exportContact {
-    static String[] loadNumbers(String path) throws Exception {
-        BufferedReader read = new BufferedReader(new FileReader(path));
-        String str;
-        String[] str2 = new String[3];
-        int i = 0;
-        while ((str = read.readLine()) != null) {
-            String result = str.replaceAll("\\D+", "");
-            System.out.println(result);
-            str2[i] = result;
-            i++;
+    // static String[] loadNumbers(String path) throws Exception {
+    // BufferedReader read = new BufferedReader(new FileReader(path));
+    // String str;
+    // String[] str2 = new String[3];
+    // int i = 0;
+    // while ((str = read.readLine()) != null) {
+    // String result = str.replaceAll("\\D+", "");
+    // System.out.println(result);
+    // str2[i] = result;
+    // i++;
+    // }
+    // return str2;
+
+    static void expContact(String path) throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\ ");
+            System.out.println(Arrays.toString(words));
         }
-        return str2;
+
+        scanner.close();
     }
+
 }
--- PhoneBook/program.java
+++ PhoneBook/program.java
@@ -2,10 +2,17 @@ import java.io.IOException;
 
 public class program {
     public static void main(String[] args) throws IOException {
-        Contact contact = importContact.getScannerNumber();
+        // Contact contact = importContact.getScannerNumber();
 
-        importContact.writeResult(contact.getContactRows(), "C:/JavaProjects/PhoneBook/book.txt");
-        importContact.writeResult(contact.getContactRow(), "C:/JavaProjects/PhoneBook/book2.txt");
+        // importContact.writeResult(contact.getContactRows(),
+        // "C:/JavaProjects/PhoneBook/book.txt");
+        // importContact.writeResult(contact.getContactRow(),
+        // "C:/JavaProjects/PhoneBook/book2.txt");
+
+        exportContact.expContact("C:/JavaProjects/PhoneBook/book.txt");
+        System.out.println("-------------------------------------");
+
+        exportContact.expContact("C:/JavaProjects/PhoneBook/book2.txt");
 
     }
 
